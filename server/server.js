2a2dae872be9e51a5820e723f82927c6992c7ffe
Akshat Shrivastava <akshatshrivastava@hotmail.com>
2016-07-09 13:33:23 -0700

    added SSL

--- server/server.js
+++ server/server.js
@@ -2,7 +2,12 @@
 var express    = require('express');
 var bodyParser = require('body-parser');
 var app        = express();
+var config = require('config');
+var crypto = require('crypto');
+var  https = require('https'); 
+var request = require('request');
 
+const HASH = config.get("hash");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -11,10 +16,12 @@ var port     = process.env.PORT || 8080; // set our port
 
 var router = express.Router();
 
-
 var path    = require("path");
 
-app.use(express.static(path.join(__dirname, './')));
+app.set('port', port);
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 
 // middleware to use for all requests
@@ -37,6 +44,29 @@ app.use("/images",  express.static(__dirname + '/images'));
 app.get('/',function(req,res){
 	res.sendfile(path.join(__dirname + '/index.html'));
 });
+
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+
+  if (!signature) {
+    // For testing, let's log an error. In production, you should throw an 
+    // error.
+    console.error("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
+
+    var expectedHash = crypto.createHmac('sha1', HASH)
+                        .update(buf)
+                        .digest('hex');
+
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
+
 
 app.use('/api', router);
 

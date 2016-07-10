b7097b5d4b72ccccfa98acdd505c9bea123b4ab2
Anurag Banerjee <anuragb2010@gmail.com>
2016-07-10 13:40:56 -0700

    Rewrite Job template

--- app/components/ListingJob.js
+++ app/components/ListingJob.js
@@ -13,11 +13,16 @@ import {
   Image,
 } from 'react-native';
 
+const moment = require('moment');
+
 class ListingJob extends Component {
   render() {
     return (
       <View style={styles.row}>
-        <Text>{this.props.item.name}</Text>
+        <Text style={styles.name}>{this.props.item.name}</Text>
+        <Text style={styles.dates}>{moment.unix(parseInt(this.props.item.time.startTime) + 1468181919).format("MMMM D")}</Text>
+        <Text style={styles.description}>{this.props.item.description}</Text>
+        <Text style={styles.rating}>{parseInt(this.props.item.score * 10 + 50)}% employer satisfaction rate</Text>
       </View>
     );
   }
@@ -38,6 +43,12 @@ const styles = StyleSheet.create({
   name: {
     color: 'black',
     fontSize: 16,
+  },
+  dates: {
+    marginBottom: 10,
+  },
+  rating: {
+    marginTop: 10,
   }
 })
 

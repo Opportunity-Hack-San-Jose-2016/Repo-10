aa45bc9d5e0af10383eb7828a6684bbdc62c7027
Ac2zoom <ac2zoom@yahoo.com>
2016-07-10 10:45:03 -0700

    Adjusted tables to just display item names

--- app/components/ListingJob.js
+++ app/components/ListingJob.js
@@ -17,7 +17,7 @@ class ListingJob extends Component {
   render() {
     return (
       <View style={styles.row}>
-        <Text>{JSON.stringify(this.props.item)}</Text>
+        <Text>{this.props.item.name}</Text>
       </View>
     );
   }
